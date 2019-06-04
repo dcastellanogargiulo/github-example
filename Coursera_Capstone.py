c58e1a72925cd1858ef0b486a5887e57209da808
dcastellanogargiulo <49429939+dcastellanogargiulo@users.noreply.github.com>
2019-06-04 19:37:41 +1000

    Python script that prints out Hello Capstone Project Course

---
+++ Coursera_Capstone.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# # Coursera Capstone Project
+
+# ####  Load required libraries:
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# #### Print out message
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
